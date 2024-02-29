ccddce64be9261f50a3632672115e2a098a6fe20
rafael nava <153205425+rafaelnavam@users.noreply.github.com>
2024-02-29 03:08:50 +0000

    generador de cartas

--- src/app.js
+++ src/app.js
@@ -2,10 +2,45 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const palos = ["♦", "♥", "♠", "♣"];
+  const valores = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let carta = document.querySelector("#card");
+  let arriba = document.querySelector("#arriba");
+  let centro = document.querySelector("#centro");
+  let abajo = document.querySelector("#abajo");
+  let boton = document.querySelector(".boton");
+
+  function generarcarta() {
+    let palosrandom = palos[Math.floor(Math.random() * palos.length)];
+    let valoresrandom = valores[Math.floor(Math.random() * valores.length)];
+
+    arriba.textContent = palosrandom;
+    centro.textContent = valoresrandom;
+    abajo.textContent = palosrandom;
+
+    if (palosrandom === "♥" || palosrandom === "♦") {
+      card.style.color = "red";
+    } else if (palosrandom === "♠" || palosrandom === "♣") {
+      card.style.color = "black";
+    }
+  }
+  boton.addEventListener("click", generarcarta);
 };
